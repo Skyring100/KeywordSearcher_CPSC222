5ddbcd3413a4970c32b4017aa63c5225ce78afd2
Skyring100 <skyolmsted22@gmail.com>
2024-04-03 17:38:52 -0700

    Started to design how data will appear on generated website

--- src/main/java/ScrapeOrganizer.java
+++ src/main/java/ScrapeOrganizer.java
@@ -1,3 +1,6 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import websites.Website;
 
 import java.io.FileWriter;
@@ -5,6 +8,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import java.util.ArrayList;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -14,7 +18,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class ScrapeOrganizer extends Thread{
     private BlockingQueue<MinedInfo> dataQueue;
     private final WebScraper[] scrapers;
-    private FileWriter htmlWriter;
+    private final Element[] tableRows = new Element[3];
     private int responsesRemaining;
     public ScrapeOrganizer(String keyword, Website[] websites, int maxResults){
         dataQueue = new LinkedBlockingQueue<>();
@@ -22,13 +26,15 @@ public class ScrapeOrganizer extends Thread{
         for(int i = 0; i < websites.length; i++){
             scrapers[i] = new WebScraper(websites[i], keyword, dataQueue, maxResults);
         }
+        //setup the rows for website, main content and link
+        for(int i = 0; i < tableRows.length; i++){
+            tableRows[i] = new Element("tr");
+        }
         responsesRemaining = websites.length * maxResults;
         try {
             Path newFilePath = Path.of("created_webpages/" + keyword + ".html");
             Files.deleteIfExists(newFilePath);
             Files.copy(Path.of("html_baseplate/basePlate.html"), newFilePath);
-            htmlWriter = new FileWriter(newFilePath.toFile());
-
         }catch (IOException e){
             throw new RuntimeException(e);
         }
@@ -55,22 +61,19 @@ public class ScrapeOrganizer extends Thread{
 
 
                 //eventually write this as "fill in the blank" HTML code but for now just write the contents of the data
-                try {
-                    htmlWriter.write("\n"+d.getWebsiteName()+" says: \n"+d.getMainContent()+"\n\nFrom "+d.getUrl()+"\n");
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+                System.out.println("\n"+d.getWebsiteName()+" says: \n"+d.getMainContent()+"\n\nFrom "+d.getUrl()+"\n");
+                addDataToPage(d);
 
                 if(responsesRemaining == 0){
                     running = false;
                 }
             }
         }
-        try {
-            htmlWriter.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
         System.out.println("---DONE COLLECTING DATA---");
+    }
+    private void addDataToPage(MinedInfo d){
+        tableRows[0].append("<th>"+d.getWebsiteName()+"</th>");
+        tableRows[1].append("<td>"+d.getMainContent()+"</td>");
+        tableRows[2].append("<td>"+d.getUrl()+"</td>");
     }
 }
